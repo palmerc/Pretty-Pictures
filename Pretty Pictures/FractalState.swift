998bf0a3cef3d5cf31497ebbbbff54ede763c75c
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-04-22 11:53:09 +0200

    Added the type field to fractal state

--- Pretty Pictures/FractalState.swift
+++ Pretty Pictures/FractalState.swift
@@ -1,9 +1,16 @@
 import Foundation
 
 
+enum FractalType {
+    case Mandelbrot
+    case Julia
+    case BurningShip
+}
+
 
 public struct FractalState
 {
+    var type: FractalType
     var iterations: Int
     var maximumIterations: Int
     var z: Complex<Double>
